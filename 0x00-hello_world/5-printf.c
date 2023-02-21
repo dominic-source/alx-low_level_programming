155017c44ccda15799dbf873323dcff39de756ea
dominic-source <cadatechnonso@gmail.com>
2023-02-21 03:28:02 -0800

    Task 5 - correction

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,12 +3,12 @@
 /**
  * main - Outputs a string
  * @void: No arguments is needed
- * Description: Outputs a string using prinf 
+ * Description: Outputs a string using prinf
  * Return: This function return zero
  */
 int main(void)
 {
-  char str = "with proper grammar, but the outcome is a piece of art,";
+char str[] = "with proper grammar, but the outcome is a piece of art,";
 
-  printf("%s", str);
+printf("%s", str);
 }
