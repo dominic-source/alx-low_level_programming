b23ef37a90faf3362f6d8af84812aa30f6549119
dominic-source <cadatechnonso@gmail.com>
2023-02-21 03:02:47 -0800

    Output a string - task 4 correction v1.2

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 
 /**
- * main Entry - point
- *
- * Return: Always 0 (Success)
+ * main - Outputs a string
+ * @void: its a void argument
+ * Description: Outputs a string  using PUTS function.
+ * Return: returns a string 0
  */
 int main(void)
 {
