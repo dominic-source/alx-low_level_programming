a3ea1ff44747dcc09711fc228ab784a60a40f375
dominic-source <cadatechnonso@gmail.com>
2023-02-21 03:23:30 -0800

    Task 5 - Output string using PRINTF

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Outputs a string
+ * @void: No arguments is needed
+ * Description: Outputs a string using prinf 
+ * Return: This function return zero
+ */
+int main(void)
+{
+  char str = "with proper grammar, but the outcome is a piece of art,";
+
+  printf("%s", str);
+}
--- 0x00-hello_world/README.md
+++ 0x00-hello_world/README.md
@@ -3,6 +3,6 @@ Task 1 - A program to run a c file without linking
 Task 2 - Generate assembly code
 Task 3 - Compiles C and generate an executable code
 Task 4 - Output string using PUTS function
-Task 5 - 
+Task 5 - Output string using the PRINTF function 
 Task 6 -
 Task 7 -
