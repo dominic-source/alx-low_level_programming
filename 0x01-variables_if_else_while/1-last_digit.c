2735df09dadcc1d34504087498002d80a3be1620
dominic-source <cadatechnonso@gmail.com>
2023-02-22 03:09:19 -0800

    Display a string with last digit- version 1.1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -18,11 +18,13 @@ n = rand() - RAND_MAX / 2;
 last_digit = (n % 10);
 
 if (last_digit > 5)
-printf("Last digit of %i is %i and is greater than 5 \n", n, last_digit);
+printf("Last digit of %i is %i and is greater than 5\n", n, last_digit);
 else if (last_digit == 0)
-printf("Last digit of %i is %i and is zero \n", n, last_digit);
+printf("Last digit of %i is %i and is 0\n", n, last_digit);
 else if (last_digit < 6 && last_digit != 0)
-printf("Last digit of %i is %i and is less \
-than 6 and not zero \n", n, last_digit);
-return (0);
+{
+printf("Last digit of %i is %i and is", n, last_digit);
+printf(" less than 6 and not 0\n");
+}
+    return (0);
 }
