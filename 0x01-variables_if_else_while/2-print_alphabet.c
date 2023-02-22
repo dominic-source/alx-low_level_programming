37ce439e8ac770176e762bc725ddd22c7b56fb3f
dominic-source <cadatechnonso@gmail.com>
2023-02-22 05:21:09 -0800

    Print all alphabet in lower case

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - display all alphabets in lowercase
+ * Description: a Function to display all alphabets in lowercase letters
+ * Return: returns a value 0
+ */
+int main(void)
+{
+char alphabets[] = "abcdefghijklmnopqrstuvwxyz";
+int i;
+
+for (i = 0; i < 27; i++)
+{
+putchar(alphabets[i]);
+}
+putchar('\n');
+return (0);
+}
