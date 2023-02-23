9d9024b51b8e715219af3de1ea5e23bdafc23ba3
dominic-source <cadatechnonso@gmail.com>
2023-02-23 03:30:59 -0800

    Print putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints out _putchar
+ * Description: Display _putchar to the standard output
+ * Return: 0
+ */
+int main(void)
+{
+char putchar[] = "_putchar";
+
+printf("%s\n", putchar);
+return (0);
+}
