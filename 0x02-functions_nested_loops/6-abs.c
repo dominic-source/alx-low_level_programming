e70c7882201d49c04a16f44267be0ba261b30e0d
dominic-source <cadatechnonso@gmail.com>
2023-02-26 09:47:40 -0800

    Task 6 - display the absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - Prints absolute value of an integer
+ * Description: This function will print out the absolute value of an integer
+ * @c: c is an integer
+ * Return: 0
+ */
+int _abs(int c)
+{
+if (c < 0)
+return (c * -1);
+else
+return (c);
+}
