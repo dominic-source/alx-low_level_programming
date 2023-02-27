85a9477dcf8150a8342d8df647a043cd45d6f39c
dominic-source <cadatechnonso@gmail.com>
2023-02-27 02:27:30 -0800

    Task 101 - prints multiples 3 or 5

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - print all multiples of 1024(excluded)
+ * Description - prints all the multiples of 1024
+ * Return: 0
+ * @void: no arguments
+ */
+int main(void)
+{
+int i = 0;
+int result = 0;
+int value = 1024 ;
+
+while (i < value)
+{
+if ((i % 3) == 0 || (i % 5)== 0)
+result += i;
+i++;
+}
+printf("%i\n", result);
+return (0);
+}
