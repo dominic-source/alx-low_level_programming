185c7b55b476462ae189af0bdd9093e45ba0f7a2
dominic-source <cadatechnonso@gmail.com>
2023-02-26 22:48:07 -0800

    Task 10 - add two integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - Adds two integers
+ * Descreption: Add function adds two integers and produces a result
+ * Return: result of addition
+ * @n: this argument is an integer and is required
+ * @m: this argument is an integer and is required
+ */
+int add(int n, int m)
+{
+int result;
+result = n + m;
+
+return (result);
+}
