b81bd73b4e34d5c8ba4ab9f9609387c957a42c11
dominic-source <cadatechnonso@gmail.com>
2023-02-26 23:27:58 -0800

    Task 11 - print all natural numbers up to 98 version 1.2

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,24 +8,27 @@
  */
 void print_to_98(int n)
 {
-  if (n < 98)
-    {
-      while (n < 98)
-	{
-	  printf("%i, ", n);
-	  n++;
-	}
-      printf("98");
-    }
-  else if (n > 98)
-    {
-      while (n > 98)
-	{
-	  printf("%i, ", n);
-	  n--;
-	}
-      printf("98");
-    }
-  putchar('\n');
-
+if (n < 98)
+{
+while (n < 98)
+{
+printf("%i, ", n);
+n++;
+}
+printf("98");
+}
+else if (n > 98)
+{
+while (n > 98)
+{
+printf("%i, ", n);
+n--;
+}
+printf("98");
+}
+else
+{
+printf("98");
+}
+putchar('\n');
 }
