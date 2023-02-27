e9ea3a4d48b37edf3da28dada2cfd1129498f4e1
dominic-source <cadatechnonso@gmail.com>
2023-02-27 06:58:11 -0800

    ask 102 - Fibonacci sequence version 1.3

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -5,21 +5,21 @@
  * Description: print the first 50 fibonacce numbers
  * Return: void
  */
-int main()
+int main(void)
 {
-int i = 1;
-int j = 2;
+unsigned i = 1;
+unsigned j = 2;
 int count = 48;
 int k = 0;
-int m;
-  
-printf("%i, %i", i, j); 
-while(k < count)
+unsigned m;
+
+printf("%ul, %ul", i, j);
+while (k < count)
 {
 m = j + i;
 i = j;
 j = m;
-printf(", %i", j);
+printf(", %ul", j);
 k++;
 }
 printf("\n");
