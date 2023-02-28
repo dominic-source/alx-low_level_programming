ae99db7764db240383a48b3ba68278db491ef589
dominic-source <cadatechnonso@gmail.com>
2023-02-28 03:28:28 -0800

    Task 10 - print triangles with hash

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,1 +1,32 @@
 #include "main.h"
+
+/**
+ * print_triangle - print triangle #
+ * Description: a function to a triangle of #
+ * Return: void
+ * @size: size is an integer
+ */
+void print_triangle(int size)
+{
+int i = 0;
+int j = size;
+int k = 0;
+while (i <= size)
+{
+j = size - i;
+k += i;
+while (j != 0)
+{
+_putchar(' ');
+j--;
+}
+
+while (k != 0)
+{
+_putchar('#');
+k--;
+}
+_putchar('\n');
+i++;
+}
+}
