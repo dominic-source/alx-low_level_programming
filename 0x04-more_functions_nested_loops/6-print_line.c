94b2e3e003fd13de54cfec93e3d75e94fdf9dc49
dominic-source <cadatechnonso@gmail.com>
2023-02-27 19:21:28 -0800

    Task 6 - print lines

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,1 +1,18 @@
 #include "main.h"
+
+/**
+ * print_line - print line
+ * Description: A function that print lines given a parameter
+ * Return: void
+ * @n: number of lines
+ */
+void print_line(int n)
+{
+while (n > 0)
+{
+_putchar('_');
+n--;
+}
+_putchar('\n');
+return;
+}
