a6d8b7c4648a4b4535fe5667126ab25b4ac030bf
dominic-source <cadatechnonso@gmail.com>
2023-02-27 20:32:56 -0800

    Task 9 - print fizz buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,1 +1,26 @@
-#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - print numbers, fizz and buzz in multiples
+ * Description: a function to print 1 to 100 replacing 3 and 5
+ * Return: void
+ * @void: no argument
+ */
+void main(void)
+{
+int i;
+printf("1");
+for (i = 2; i < 101; i++)
+{
+if (((i % 3) == 0) && ((i % 5) == 0))
+printf(" %s", "FizzBuzz");
+else if ((i % 3) == 0)
+printf(" %s", "Fizz");
+else if ((i % 5) == 0)
+printf(" %s", "Buzz");
+else
+printf(" %i", i);
+}
+printf("\n");
+return;
+}
