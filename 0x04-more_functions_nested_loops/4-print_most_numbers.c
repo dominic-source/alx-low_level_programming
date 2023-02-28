bcc1e493f30643e5fb9cd7b2dc9aeeba5ce1cae3
dominic-source <cadatechnonso@gmail.com>
2023-02-27 18:47:54 -0800

    Task 4 - Print numbers except 2 and 4 version 1.2

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_numbers - print numbers from 0 to 9
+ * print_most_numbers - print numbers from 0 to 9
  * Description: A function to print all numbers from 0 to 9
  * Returns: void
  * @void: no arguments
@@ -11,7 +11,7 @@ void print_most_numbers(void)
 int i = 0;
 while (i <= 9)
 {
-if(i != 2 && i != 4)
+if (i != 2 && i != 4)
 _putchar('0' + i);
 i++;
 }
