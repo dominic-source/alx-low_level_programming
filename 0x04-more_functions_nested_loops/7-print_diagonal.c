939d9fc19d1024b6da6e2233400f47346b8a0301
dominic-source <cadatechnonso@gmail.com>
2023-02-27 19:45:51 -0800

    Task 7 - print diagonals

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,1 +1,31 @@
 #include "main.h"
+
+
+/**
+ * print_diagonal - print line
+ * Description: A function that print lines given a parameter
+ * Return: void
+ * @n: number of lines
+ */
+void print_diagonal(int n)
+{
+int i = 0;
+while (n > 0)
+{
+while (i < n)
+{
+int j = 0;
+while (j < i)
+{
+_putchar(' ');
+j++;
+}
+_putchar('\\');
+_putchar('\n');
+i++;
+}
+n--;
+}
+_putchar('\n');
+return;
+}
