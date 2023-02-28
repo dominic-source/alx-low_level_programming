da598f3fc0c2d896a3eaced99c4a43c3783079d4
dominic-source <cadatechnonso@gmail.com>
2023-02-27 19:04:33 -0800

    Task 5 - print numbers 10 times

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,1 +1,25 @@
 #include "main.h"
+
+/**
+ * more_numbers - print numbers from 0 to 14
+ * Description: A function to print all numbers from 0 to 14
+ * Returns: void
+ * @void: no arguments
+ */
+void more_numbers(void)
+{
+int j = 0;
+while (j < 10)
+{
+int i = 0;
+while (i <= 14)
+{
+if(i > 10)
+_putchar('0' + (i / 10));
+_putchar('0' + (i % 10));    
+i++;
+}
+j++;
+_putchar('\n');
+}
+}
