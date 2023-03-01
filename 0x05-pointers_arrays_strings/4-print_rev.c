4eee996717e2d09ec4b192a0d9177afb6a7f0223
dominic-source <cadatechnonso@gmail.com>
2023-03-01 09:04:00 -0800

    Task 4 - print in reverse order

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_rev - print a string in reverse order
+ * @s: char argument
+ * Return: Nothing
+ */
+void print_rev(char *s)
+{
+int i = 0;
+int length = 0;
+int j = 0;
+while (i > -1)
+{
+if (*(s + i) != '\0')
+{
+length++;
+i++;
+}
+else
+{
+while (j <= length)
+{
+_putchar(*(s + (length - j)));
+j++;
+}
+_putchar('\n');
+return;
+}
+}
+}
