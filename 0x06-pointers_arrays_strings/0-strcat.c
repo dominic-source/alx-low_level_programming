718468e216e854d49a5eb11da13ef3501f93f3a6
dominic-source <cadatechnonso@gmail.com>
2023-03-02 05:33:11 -0800

    Task 0 - concatenate strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenate strings
+ *Return: pointer to a char
+ *@dest: destination of the string
+ *@src: src of string
+ */
+char *_strcat(char *dest, char *src)
+{
+int i = 0;
+int len_dest = 0;
+int len_src = 0;
+int j, k;
+
+while (i > -1)
+{
+if (*(src + i) != '\0' || *(src + i) != '\0')
+{
+if (*(src + i) != '\0')
+len_src++;
+if (*(dest + i) != '\0')
+len_dest++;
+i++;
+}
+else
+{
+j = len_dest;
+k = 0;
+while (j < (len_dest + len_src))
+{
+*(dest + j) = *(src + k);
+j++;
+k++;
+}
+return (dest);
+}
+}
+return (dest);
+}
