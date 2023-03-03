55887bfe12fcf7f57cbba5d2d6081d754a0093ad
dominic-source <cadatechnonso@gmail.com>
2023-03-03 03:18:49 -0800

    Task 3 - compare string

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,14 +9,20 @@
 int _strcmp(char *s1, char *s2)
 {
 int i = 0;
-while (i > -1)
+while (i > -1 && *(s1 + i) != '\0' && *(s2 + i) != '\0')
 {
 if (*(s1 + i) < *(s2 + i) || *(s1 + i) > *(s2 + i))
 {
 return (*(s1 + i) - *(s2 + i));
 }
 else
-return (*(s1 + i) - *(s2 + i));
+{
+i++;
 }
+}
+if (*(s1 + i) == '\0')
+return (0 - *(s2 + i));
+if (*(s2 + i) == '\0')
+return (*(s1 + i)-0);
 return (1);
 }
