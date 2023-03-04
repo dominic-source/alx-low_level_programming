dca4ae6ea53f6c8dd8e722d5abfcc43f45d9064b
dominic-source <cadatechnonso@gmail.com>
2023-03-04 13:38:12 -0800

    Task 100 - rot13 encryption

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -3,10 +3,26 @@
 /**
  * rot13 - encodes a string to rot13
  * Return: a pointer to a char
- * @str: a string with a pointer
+ * @s: a string with a pointer
  */
-char *rot13(char *str)
+char *rot13(char *s)
 {
+int i;
 
-return (str);
+i = 0;
+while (*(s + i) != '\n')
+{
+if ((*(s + i) > 64 && *(s + i) < 78) || (*(s + i) > 96 && *(s + i) < 110))
+{
+*(s + i) = *(s + i)+13;
+}
+else if ((*(s + i) > 77 && *(s + i) < 91) ||
+	 (*(s + i) > 109 && *(s + i) < 123))
+{
+*(s + i) = *(s + i)-13;
+}
+i++;
+}
+
+return (s);
 }
