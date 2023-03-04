b72c905dd161b42d87791f49a3866b510497e7e9
dominic-source <cadatechnonso@gmail.com>
2023-03-04 09:03:46 -0800

    Task 101 - print integer

--- 0x06-pointers_arrays_strings/101-main.c
+++ 0x06-pointers_arrays_strings/101-main.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <limits.h>
 
 /**
  * main - check the code
--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -7,12 +7,15 @@
  */
 void print_number(int n)
 {
+
 if (n < 0)
 {
-n = -n;
 _putchar('-');
+n *= -1;
 }
+
 if (n / 10)
 print_number(n / 10);
-_putchar('0' + (n % 10));
+
+_putchar((n % 10) + '0');
 }
