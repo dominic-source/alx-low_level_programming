3157744b8d4fa4e4964dd643aaaf120e7c13ac58
dominic-source <cadatechnonso@gmail.com>
2023-03-05 12:56:42 -0800

    Task 100 - prime factor

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - print prime factor
+ * Return: Nothing
+ * @void: No arguments
+ */
+int main(void)
+{
+long int i = 612852475143;
+long int value = 1;
+long int j;
+long int k;
+for (j = 1; j < i; j++)
+{
+if ((i % j) == 0)
+{  	  
+for (k = 2; k < j; k++)
+{
+if ((j % k) == 0 )
+{
+break;
+}
+else
+{
+value = j;
+}
+}
+}
+}
+printf("%li\n", value);
+return (0);
+}
+
