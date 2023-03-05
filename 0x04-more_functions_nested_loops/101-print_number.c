8a8304e77a0171419041bf2eff3de671b4624e55
dominic-source <cadatechnonso@gmail.com>
2023-03-05 05:11:23 -0800

    Task 101 - print numbers

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_number - print number
+ * Return: Nothing
+ * @n: an integer
+ */
+void print_number(int n)
+{
+
+if (n < 0)
+{
+_putchar('-');
+n *= -1;
+}
+
+if (n / 10)
+print_number(n / 10);
+
+_putchar((n % 10) + '0');
+}
