b79dfa9a83609208a47c5e925de5afc21284ae18
dominic-source <cadatechnonso@gmail.com>
2023-03-05 09:51:44 -0800

    Task 102 - print two two numbers combination

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,20 +8,29 @@
  */
 int main(void)
 {
-int i, j;
+int i, j, k, l;
 for (i = 0; i < 10; i++)
 {
 for (j = 0; j < 10; j++)
 {
-if (i < j)
+for (k = 0; k < 10; k++)
 {
-if (j != 1)
+for (l = 0; l < 10; l++)
+{
+if (((k + l) > (i + j) && k <= i) || i < k)
+{
+if (!(i == 0 && k == 0 && j == 0 && l == 1))
 {
 putchar(',');
 putchar(' ');
 }
 putchar('0' + i);
 putchar('0' + j);
+putchar(' ');
+putchar('0' + k);
+putchar('0' + l);
+}
+}
 }
 }
 }
