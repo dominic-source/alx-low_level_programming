60b127a47c5c675367656d9838f3cbbc4b3dc446
dominic-source <cadatechnonso@gmail.com>
2023-03-07 07:36:53 -0800

    Task 5 - search for a string

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -8,23 +8,19 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
-int i = 0, j;
-while (i > -1)
+int i, j;
+
+for (i = 0; haystack[i] != '\0'; i++)
 {
-j = 0;
-while (j > -1)
+for (j = 0; needle[j] != '\0'; j++)
 {
-if (needle[i] != haystack[j + i])
+if (needle[j] != haystack[i + j])
+{
 break;
-else if (haystack[j] == '\0')
-{
-return (haystack + j);
 }
-j++;
 }
-if (needle[i] == '\0')
-return (NULL);
-i++;
+if (!needle[j])
+return (&haystack[i]);
 }
 return (NULL);
 }
