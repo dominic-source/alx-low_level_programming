5231f7f2c6b58c94ae0a6bf1b3c40a180f967512
dominic-source <cadatechnonso@gmail.com>
2023-03-11 00:57:36 -0800

    Task 0 - print program name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - a function to print arguments
+ * Return: 0
+ * @argc : argument count
+ * @argv: arguments
+ */
+int main(int argc, char *argv[])
+{
+if (argc >= 1)
+printf("%s\n", argv[0]);
+return (0);
+}
