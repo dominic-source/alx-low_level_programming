3160f19cd76f1df261e6705152083af05874d5b4
dominic-source <cadatechnonso@gmail.com>
2023-03-14 10:31:01 -0700

    Task 2 - concatenate two string

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,21 +12,26 @@
 char *str_concat(char *s1, char *s2)
 {
 char *my_concat;
-size_t i, j = 0;
-
-my_concat = malloc(sizeof(char) * (strlen(s1) + strlen(s2) + 1));
+size_t i = 0, j = 0, len_1, len_2;
 
 if (s1 == NULL)
-s1 = '\0';
+len_1 = 0;
+else
+len_1 = strlen(s1);
 if (s2 == NULL)
-s2 = '\0';
+len_2 = 0;
+else
+len_2 = strlen(s2);
+
+my_concat = malloc(sizeof(char) * (len_1 + len_2 + 1));
+
 if (my_concat == NULL)
 return (NULL);
-for (i = 0; i < strlen(s1); i++)
+for (i = 0; i < len_1; i++)
 {
 my_concat[i] = s1[i];
 }
-while (j < strlen(s2))
+while (j < len_2)
 {
 my_concat[i + j] = s2[j];
 j++;
