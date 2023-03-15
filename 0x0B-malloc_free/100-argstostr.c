eba15a70aeee956e088302d382072679feae816d
dominic-source <cadatechnonso@gmail.com>
2023-03-15 08:46:47 -0700

    Task 100 - concatenate string with new line

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -19,7 +19,7 @@ if (ac == 0 || av == NULL)
 return (NULL);
 
 
-new_str = malloc(sizeof(char) * ac * (ac - 2));
+ new_str = malloc(sizeof(char) * ac * ac - (2 * ac));
 if (new_str == NULL)
 return (NULL);
 
