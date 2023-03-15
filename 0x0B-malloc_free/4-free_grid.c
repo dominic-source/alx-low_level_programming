6e090fecd25725cfd18f58029f6f5124ccc7ccc0
dominic-source <cadatechnonso@gmail.com>
2023-03-15 03:40:22 -0700

    Task 4 - free memory

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * free_grid - free the allocated memory
+ * @grid: pointer to the grid function
+ * @height: height of the grid function
+ */
+void free_grid(int **grid, int height)
+{
+int i;
+for (i = 0; i < height; i++)
+free(grid[i]);
+free(grid);  
+}
