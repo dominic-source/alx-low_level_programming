d0e83c489a7d95296668fe931787ad1eb359436b
dominic-source <cadatechnonso@gmail.com>
2023-03-15 00:09:57 -0700

    Task 3 - allocated memory for a 2D array

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * alloc_grid - returns a pointer to a two dimensional array
+ * @width: width of the array, an integer
+ * @height: height of the array, an integer
+ * Return: a pointer to an array
+ */
+int **alloc_grid(int width, int height)
+{
+int **my_dim_arr;
+int i, j;
+
+if (width <= 0 || height <= 0)
+return (NULL);
+
+my_dim_arr = malloc(sizeof(*my_dim_arr) * height);
+
+if (my_dim_arr == NULL)
+return (NULL);
+
+for (i = 0; i < height; i++)
+{
+my_dim_arr[i] = malloc(sizeof(**my_dim_arr) * width);
+if (my_dim_arr[i] == NULL)
+return (NULL);
+
+for (j = 0; j < width; j++)
+my_dim_arr[i][j] = 0;
+}
+return (my_dim_arr);
+}
