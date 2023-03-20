9c93ff4b84a729378eabf6101fda43c316e7ae3d
dominic-source <cadatechnonso@gmail.com>
2023-03-19 23:37:45 -0700

    Task 1- completed

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -17,23 +17,29 @@ char *str;
 unsigned int i = 0, j = 0;
 unsigned int size;
 
-size = strlen(s1) + n + 1;
-if (n >= strlen(s2))
+
+if (s2 == NULL)
+n = 0;
+else if (n >= strlen(s2))
 n = strlen(s2);
+
+size = strlen(s1) + n + 1;
 
 str = malloc(sizeof(char) * size);
 str = realloc(str, size);
-
 if (str == NULL || n >= INT_MAX)
 return (NULL);
 
 for (i = 0; i < strlen(s1) && s1[i] != '\0'; i++)
 str[i] = s1[i];
 
+if (s2 != NULL)
+{
 for (j = 0; j < n && s2[j] != '\0'; j++)
 {
 str[i] = s2[j];
 i++;
+}
 }
 str[i] = '\0';
 return (str);
