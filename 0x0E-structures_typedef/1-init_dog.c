4af3a906d995e500e518f3cb773e2127bde23a93
dominic-source <cadatechnonso@gmail.com>
2023-03-21 10:19:04 -0700

    Task 1 - debug

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -12,13 +12,8 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-d = malloc(sizeof(struct dog));
-if (d == NULL)
-return;
-
 d->name = name;
-d->owner = owner;
 d->age = age;
-
+d->owner = owner;
 return;
 }
