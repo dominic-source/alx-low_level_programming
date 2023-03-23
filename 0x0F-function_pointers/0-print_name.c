5394f9f8980140222ed6a4aea582ce63ea72ad53
dominic-source <cadatechnonso@gmail.com>
2023-03-23 00:35:42 -0700

    Task 0 - print name debugging

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,4 @@
 #include <stddef.h>
-#include "function_pointers.h"
 #include <stdlib.h>
 #include <string.h>
 /**
@@ -12,6 +11,8 @@ void print_name(char *name, void (*f)(char *))
 char *n_name;
 n_name = malloc(sizeof(char) * (strlen(name) + 1));
 if (n_name == NULL)
+return;
+if (f == NULL)
 return;
 
 strcpy(n_name, name);
---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,12 @@
+
+/**
+ * int_index - searches for an integer
+ * @array: array of integers
+ * @size: size of array
+ * @cmp: pointer to a function
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+
+
+}
