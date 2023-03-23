55e51a017325e9f0c9a56c10775e0037314cd72c
dominic-source <cadatechnonso@gmail.com>
2023-03-23 04:07:00 -0700

    Task 3 - debugging

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -34,12 +34,6 @@ exit(100);
 my_fun = get_op_func(argv[2]);
 result = my_fun(atoi(argv[1]), atoi(argv[3]));
 
-if (my_fun == NULL)
-{
-printf("Error\n");
-exit(101);
-}
-
 printf("%i\n", result);
 return (0);
 }
