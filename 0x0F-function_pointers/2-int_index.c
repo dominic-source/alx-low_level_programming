b334e5970ed2f1058346e2b22f9af15fde9566ba
dominic-source <cadatechnonso@gmail.com>
2023-03-23 00:53:52 -0700

    Task 2 - search string

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,12 +1,25 @@
+#include <stddef.h>
 
 /**
  * int_index - searches for an integer
  * @array: array of integers
  * @size: size of array
  * @cmp: pointer to a function
+ * Return: 1 or -1
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
+int i;
+if (array == NULL)
+return (1);
+if (cmp == NULL)
+return (1);
+if (size <= 0)
+return (-1);
 
+for (i = 0; i < size; i++)
+if (cmp(array[i]))
+return (i);
 
+return (-1);
 }
