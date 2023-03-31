2ca19e7b883ed887f3cac7805edfa31a1ab3306a
dominic-source <cadatechnonso@gmail.com>
2023-03-31 15:36:56 -0700

    Correct spaces for task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,16 +7,17 @@
  */
 size_t print_list(const list_t *h)
 {
-size_t count = 0;
+	size_t count = 0;
 
-while (h != NULL)
-{
-if (h->str != NULL)
-printf("[%i] %s\n", h->len, h->str);
-else
-printf("[0] (nil)\n");
-count++;
-h = h->next;
-}
-return (count);
+	while (h != NULL)
+	{
+		if (h->str != NULL)
+			printf("[%i] %s\n", h->len, h->str);
+		else
+			printf("[0] (nil)\n");
+		count++;
+		h = h->next;
+
+	}
+	return (count);
 }
