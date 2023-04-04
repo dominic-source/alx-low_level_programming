569e6ea391068c08d37b9491dc2049044df6bc5d
dominic-source <cadatechnonso@gmail.com>
2023-04-04 02:57:34 -0700

    In search of bugs

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -8,8 +8,8 @@ void free_listint2(listint_t **head)
 {
 	listint_t *hold;
 
-	if ((*head)->next == NULL)
-		*head = NULL;
+	if (*head == NULL)
+		return;
 	while ((*head)->next != NULL)
 	{
 		hold = (*head)->next;
