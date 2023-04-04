cc2143867e5fc8938fad2f770dd89843e092b244
dominic-source <cadatechnonso@gmail.com>
2023-04-04 04:28:40 -0700

    Insert node anywhere

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -17,10 +17,20 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (new == NULL)
 		return (NULL);
 	hold = head;
-	for (i = 0; (i < idx - 1) && hold != NULL; i++)
+	if (idx == 0)
+	{
+		new->n = n;
+		new->next = *head;
+		*head = new;
+		return (new);
+	}
+	for (i = 0; (i < idx - 1) && (*hold)->next != NULL; i++)
 		hold = &((*hold)->next);
-	if (hold == NULL && i < (idx - 1))
+	if ((*hold)->next == NULL && i < (idx - 1))
+	{
+		free(new);
 		return (NULL);
+	}
 	new->n = n;
 	new->next = (*hold)->next;
 	(*hold)->next = new;
