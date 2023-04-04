232be71371d6f2d95b9cc5b316f45f81c950f0c2
dominic-source <cadatechnonso@gmail.com>
2023-04-04 08:30:52 -0700

    Delete node

--- 0x13-more_singly_linked_lists/10-main.c
+++ 0x13-more_singly_linked_lists/10-main.c
@@ -23,7 +23,7 @@ int main(void)
     add_nodeint_end(&head, 1024);
     print_listint(head);
     printf("-----------------\n");
-    delete_nodeint_at_index(&head, 5);
+    delete_nodeint_at_index(NULL, 0);
     print_listint(head);
     printf("-----------------\n");
     delete_nodeint_at_index(&head, 0);
