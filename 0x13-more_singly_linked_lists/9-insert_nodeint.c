a1685f27c5d17c090294245cf3b3c126155d470e
dominic-source <cadatechnonso@gmail.com>
2023-04-04 04:00:53 -0700

    Insert new node at index

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -5,12 +5,24 @@
  * @head: head node
  * @idx: index to insert node
  * @n: number to insert
+ * Return: the address of the newly add node
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
+	listint_t **hold;
+	listint_t *new;
+	unsigned int i;
 
-
-
-
-
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+	hold = head;
+	for (i = 0; (i < idx - 1) && hold != NULL; i++)
+		hold = &((*hold)->next);
+	if (hold == NULL && i < (idx - 1))
+		return (NULL);
+	new->n = n;
+	new->next = (*hold)->next;
+	(*hold)->next = new;
+	return (new);
 }
