6fce790c48662801fe153046e6f6194cc2e31b00
dominic-source <cadatechnonso@gmail.com>
2023-04-05 03:26:14 -0700

    task 5 debugging

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+
+/**
+ * print_listint_safe - print list in safe mode
+ * @head: head of elements
+ * Return: number of nodes
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+
+
+
+
+}
