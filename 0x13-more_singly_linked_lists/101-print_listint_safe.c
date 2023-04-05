ed283c9e1dcdc62010ec6fe3727ae19cd6a099bd
dominic-source <cadatechnonso@gmail.com>
2023-04-05 12:55:26 -0700

    print linked list safer version

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -10,14 +10,18 @@ size_t print_listint_safe(const listint_t *head)
 	const listint_t *hold;
 	size_t sum = 0;
 
+	if (head == NULL)
+		return (sum);
 	hold = head;
+
 	while (hold->next != NULL)
 	{
 		if (hold < hold->next)
 		{
+			sum++;
 			printf("[%p] %i\n", (void *)&(hold->next), hold->n);
 			printf("-> [%p] %i\n", (void *)&(hold->next->next), 98);
-			exit(98);
+			return (98);
 		}
 		sum++;
 		printf("[%p] %i\n", (void *)&(hold->next), hold->n);
