990bc4e375bf94847cda56b11591106e46c20248
dominic-source <cadatechnonso@gmail.com>
2023-04-07 12:49:31 -0700

    Convert binary to decimal

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,52 @@
+#include "main.h"
+#include <math.h>
+#include <stdlib.h>
+
+/**
+ * binary_to_uint - convert binary to unsigned integer
+ * @b: binary string to convert
+ * Return: unsigned integer
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int sum = 0;
+	int i = -1;
+	int p = -1;
+	int stop = 0;
+
+	if (b == NULL)
+		return (0);
+	sum += recursive_conv(b, &sum, &i, &p, &stop);
+	if (stop)
+		return (0);
+	return (sum);
+}
+
+/**
+ * recursive_conv - convert binary to decimal
+ * @b: pointet to the binary string
+ * @sum: result of the conversion
+ * @i: index of each char
+ * @p: power for the char
+ * @stop: stops recursion encoutering a non 1 or 0
+ * Return: total sum to give result
+ */
+int recursive_conv(const char *b, unsigned int *sum, int *i, int *p, int *stop)
+{
+	(*i)++;
+
+	/* base case */
+	if (b[(*i)] == '\0')
+		return (0);
+	if (b[*i] != '0' && b[*i] != '1')
+	{
+		*stop = 1;
+		return (0);
+	}
+
+	*sum += recursive_conv(b, sum, i, p, stop);
+	(*p)++;
+	(*i)--;
+
+	return ((b[*i] == '0' ? 0 : 1) * (1 << (*p)));
+}
---
+++ 0x14-bit_manipulation/README.md
@@ -0,0 +1,7 @@
+Task 0 - a function that converts a binary number to an unsigned int
+Task 1 - a function that prints the binary representation of a number.
+Task 2 - a function that returns the value of a bit at a given index.
+Task 3 - a function that sets the value of a bit to 1 at a given index.
+Task 4 - a function that sets the value of a bit to 0 at a given index.
+Task 5 - a function that returns the number of bits you would need to flip.
+Task 100 - a function that checks the endianness.
