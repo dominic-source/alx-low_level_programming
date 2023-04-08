ce1d977db57c07a7efbb6bc4f4028b2389742903
dominic-source <cadatechnonso@gmail.com>
2023-04-08 02:37:59 -0700

    get bit at a particular index

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -11,10 +11,6 @@ int get_bit(unsigned long int n, unsigned int index)
 {
 	int value = (n & (1u << (index)));
 
-	int test = (n & ((~0x00) << (index)));
-
-	if (test == 0)
-		return (-1);
-
+	
 	return (value > 0 ? 1 : 0);
 }
