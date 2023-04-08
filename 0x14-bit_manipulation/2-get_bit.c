426fe083f4aade68e7a699ce582e8402063d4636
dominic-source <cadatechnonso@gmail.com>
2023-04-08 03:06:12 -0700

    Set bit at an index

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -11,5 +11,7 @@ int get_bit(unsigned long int n, unsigned int index)
 {
 	int value = (n & (1u << (index)));
 
+	if (index > sizeof(unsigned long int) * 8)
+		return (-1);
 	return (value > 0 ? 1 : 0);
 }
