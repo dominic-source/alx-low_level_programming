3f48154ce700b5e7929eb536f5091f66b1323edc
dominic-source <cadatechnonso@gmail.com>
2023-04-10 14:12:44 -0700

    Read and write to stdout

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,41 @@
+#include <fcntl.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+
+/**
+ * read_textfile - read text file
+ * @filename: name of of file
+ * @letters: the number of letters to read
+ * Return: ssize_t
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	ssize_t cnt;
+	ssize_t cnt_2;
+	char *buffer;
+
+	buffer = malloc(letters);
+	if (buffer == NULL)
+		return (0);
+
+	if (filename == NULL)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+	if (fd == EBADF || fd == -1)
+		return (0);
+
+	cnt = read(fd, buffer, letters);
+	if (cnt == -1)
+		return (0);
+
+	cnt_2 = write(1, buffer, letters);
+	if (cnt_2 == -1)
+		return (0);
+
+	free(buffer);
+	return (cnt);
+}
