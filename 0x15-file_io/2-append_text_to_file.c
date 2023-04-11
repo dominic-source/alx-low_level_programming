19a6b210d9685aa61805991130f55544e9fdb6d5
dominic-source <cadatechnonso@gmail.com>
2023-04-10 21:58:41 -0700

    Created folder

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,12 @@
+#includ "main.h"
+
+/**
+ * append_text_to_file - append text to file
+ * @filename: name of file
+ * @text_content: content of file
+ * Return: 1 or -1
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+return (1);
+}
