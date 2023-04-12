598dc054de897526042839f76acd1d99d7cf59cc
dominic-source <cadatechnonso@gmail.com>
2023-04-12 07:43:13 -0700

    Debugged task 3 v10

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -11,35 +11,42 @@ int main(int argc, char *argv[])
 	char *error = "Usage: cp file_from file_to\n";
 	char *error_2 = "Error: Can't read from file ";
 	char *error_3 = "Error: Can't write to file ";
-	int fd_1, fd_2, fd_3, cnt;
+	ssize_t fd_1, fd_2, fd_3, cnt_2, cnt;
 	char buffer[M_BUFSIZ];
+	int state = 0;
 
 	if (argc != 3)
 	{
 		write(STDERR_FILENO, error, strlen(error));
 		exit(97);
 	}
+
+	/* open files */
 	fd_1 = open(argv[1], O_RDWR);
-	if (fd_1 == -1 || fd_1 == ENOENT || fd_1 == EACCES)
+
+	fd_3 = open(argv[2], O_RDWR);
+	if (fd_3 == -1)
+		state = 1;
+
+	fd_2 = open(argv[2], O_CREAT | O_RDWR | O_TRUNC, 0664);
+	if (state)
+		chmod(argv[2], 0664);
+	/* copy's the file to another file */
+	while ((cnt = read(fd_1, buffer, M_BUFSIZ)) != 0)
+		cnt_2 = write(fd_2, buffer, cnt);
+	if (cnt == -1)
 	{
 		_write_err(argv[1], error_2);
 		exit(98);
 	}
-	fd_3 = open(argv[2], O_TRUNC | O_RDWR);
-	close(fd_3);
-	fd_2 = open(argv[2], O_CREAT | O_RDWR, 0664);
-	if (fd_2 == -1 || fd_2 == EACCES || fd_2 == EBADF || fd_2 == EDQUOT)
+	if (cnt_2 == -1)
 	{
 		_write_err(argv[2], error_3);
 		exit(99);
 	}
-	/* copy's the file to another file */
-
-	while ((cnt = read(fd_1, buffer, M_BUFSIZ)) != 0)
-		write(fd_2, buffer, M_BUFSIZ);
-
+	/* close the files */
+	close(fd_3);
 	_close(fd_1, fd_2);
-
 	return (0);
 }
 
@@ -68,9 +75,6 @@ int _close(ssize_t fd_1, ssize_t fd_2)
 		_write_err((char *)fd_2, error_4);
 		exit(100);
 	}
-
-
-
 	return (cl_2);
 }
 
