ed558ef493b4b8c2d80e51a44377af2a25d842d9
dominic-source <chinonsodomnic@gmail.com>
2023-05-06 03:32:06 -0700

    debugged realloc function

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -5,44 +5,46 @@
  * @ptr: pointer to a previously allocated memory
  * @old_size: the old size
  * @new_size: the new size
+ * Return: void pointer to the allocated memory
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-char *new_ptr;
-int *_ptr;
-unsigned int i;
-_ptr = (int *)ptr;
+	char *new_ptr;
+	int *_ptr;
+	unsigned int i;
 
-if (old_size == new_size)
-return (ptr);
-if (ptr == NULL)
-{
-new_ptr = malloc(new_size);
-if (new_ptr == NULL)
-return (NULL);
-}
-else if (new_size == 0 && _ptr != NULL)
-{
-free(ptr);
-return (NULL);
-}
-else if (new_size > old_size)
-{
-new_ptr = malloc(old_size + sizeof(*new_ptr));
-if (new_ptr == NULL)
-return (NULL);
-for (i = 0; i < (old_size / sizeof(*new_ptr)); i++)
-new_ptr[i] = _ptr[i];
-free(ptr);
-}
-else
-{
-new_ptr = malloc(new_size);
-if (new_ptr == NULL)
-return (NULL);
-for (i = 0; i < (new_size / sizeof(*new_ptr)); i++)
-new_ptr[i] = _ptr[i];
-free(ptr);
-}
-return ((void *)new_ptr);
+	_ptr = (int *)ptr;
+
+	if (old_size == new_size)
+		return (ptr);
+	if (ptr == NULL)
+	{
+		new_ptr = malloc(new_size);
+		if (new_ptr == NULL)
+			return (NULL);
+	}
+	else if (new_size == 0 && _ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	else if (new_size > old_size)
+	{
+		new_ptr = malloc(new_size);
+		if (new_ptr == NULL)
+			return (NULL);
+		for (i = 0; i < (old_size / sizeof(new_ptr)); i++)
+			new_ptr[i] = _ptr[i];
+		free(ptr);
+	}
+	else
+	{
+		new_ptr = malloc(new_size);
+		if (new_ptr == NULL)
+			return (NULL);
+		for (i = 0; i < (new_size / sizeof(new_ptr)); i++)
+			new_ptr[i] = _ptr[i];
+		free(ptr);
+	}
+	return ((void *)new_ptr);
 }
