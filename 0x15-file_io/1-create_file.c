89df7d14c0c9f977fc49a9f83c8daa3322d350f0
dominic-source <chinonsodomnic@gmail.com>
2023-05-08 06:30:00 -0700

    debugged file creation v2

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,7 +17,7 @@ int create_file(const char *filename, char *text_content)
 	fd2 = open(filename, O_RDWR);
 	if (fd2 == -1)
 	{
-		close (fd2);
+		close(fd2);
 		/* if the file does not exist */
 		fd = open(filename, O_RDWR | O_CREAT, 0600);
 		if (fd == -1)
@@ -25,7 +25,7 @@ int create_file(const char *filename, char *text_content)
 	}
 	else
 	{
-		close (fd2);
+		close(fd2);
 		/* if the file already exist */
 		fd = open(filename, O_RDWR | O_TRUNC, 0);
 		if (fd == -1)
@@ -37,7 +37,7 @@ int create_file(const char *filename, char *text_content)
 		if (wr == -1)
 			return (-1);
 	}
-	close (fd);
+	close(fd);
 	if (fd2 == -1)
 		chmod(filename, 0600);
 	return (1);
