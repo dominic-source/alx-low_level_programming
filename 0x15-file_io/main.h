448f0bd34e27e237f0325c11edcc3cf8ff060e0e
dominic-source <chinonsodomnic@gmail.com>
2023-05-09 23:44:18 -0700

    read elf file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -7,9 +7,12 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/stat.h>
+#include <elf.h>
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 int close_all(int fd, int fd2, int fd3);
-
+void magic(Elf32_Ehdr *ehd);
+void abi_version(Elf32_Ehdr *ehd);
+void type_entry(Elf32_Ehdr *ehd);
 #endif
