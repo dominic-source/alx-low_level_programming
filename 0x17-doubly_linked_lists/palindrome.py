2852348258053dab722c18f4ee104470d1257fe2
dominic-source <chinonsodomnic@gmail.com>
2023-06-16 10:54:16 -0700

    find the largest palindrome when two 3 digit numbers are multiplied v1.4

--- 0x17-doubly_linked_lists/palindrome.py
+++ 0x17-doubly_linked_lists/palindrome.py
@@ -11,13 +11,14 @@ def detect(num):
 
 def multiple():
     number = 0
-    for i in range(100, 1000):
-        for j in range(100, 1000):
+    for i in range(550, 1000):
+        for j in range(550, 1000):
             result = i * j
             if detect(result) is not None:
-                number = detect(result)
-                hold = i, j
-    print(number)
-    print(hold)
+                if number < detect(result):
+                    number = detect(result)
+                    hold = i, j
+    print(number, hold)
+
 if __name__ == '__main__':
     multiple()
