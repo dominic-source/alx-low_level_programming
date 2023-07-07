6c9abb1fe1335919af22be8b28872215a29cb21a
dominic-source <chinonsodomnic@gmail.com>
2023-07-07 12:08:28 +0100

    debug hash table creation v3

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -11,11 +11,12 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *my_table;
 	unsigned long int i;
 
-	my_table = malloc(sizeof(hash_table_t));
+	my_table = (hash_table_t *)malloc(sizeof(hash_table_t));
 	if (my_table == NULL)
 		return (NULL);
 	my_table->size = size;
-	my_table->array = calloc(my_table->size, sizeof(hash_node_t));
+	my_table->array = (hash_node_t **)calloc(my_table->size, sizeof(hash_node_t));
+
 	if (my_table->array == NULL)
 	{
 		free(my_table);
