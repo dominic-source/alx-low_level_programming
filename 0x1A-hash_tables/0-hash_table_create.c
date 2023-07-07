c91d37a6c6cdfe8280928b3ca0d1550c9055594a
dominic-source <chinonsodomnic@gmail.com>
2023-07-07 11:51:19 +0100

    create the hash table

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,22 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - This will create the hash table
+ * @size: This is the size of the hash table we want to create
+ *
+ * Return: Returns the created hash table
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *my_table;
+
+	my_table = malloc(sizeof(hash_table_t));
+	if (my_table == NULL)
+		return (NULL);
+	my_table->size = size;
+	my_table->array = calloc(size, sizeof(hash_node_t));
+	if (my_table->array == NULL)
+		return (NULL);
+	return (my_table);
+}
+
---
+++ 0x1A-hash_tables/README.md
@@ -0,0 +1,6 @@
+Task 0 - Create the hash table
+
+Task 1 - Write a hash function
+
+Task 2 - write a function that gives an index for keys
+
