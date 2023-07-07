dc2e8367edd35e2560a8bf1adfb9df290b2d18f6
dominic-source <chinonsodomnic@gmail.com>
2023-07-07 14:07:29 -0700

    get data from hash table

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,28 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - get the value and key if exist
+ * @ht: the hash table
+ * @key: the key to the item to retrieve
+ * Return: the value or NULL
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	hash_node_t *current;
+	unsigned long int index;
+
+	index = key_index((unsigned const char *)key, ht->size);
+
+	if (ht->array[index] == NULL)
+		return (NULL);
+	else if (ht->array[index]->next == NULL)
+		return (ht->array[index]->value);
+
+	current = ht->array[index];
+	while (current != NULL)
+	{
+		if (strcmp(key, current->key) == 0)
+			return (current->value);
+	}
+	return (NULL);
+}
