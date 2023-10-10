a3b607fc370b8e1627045bf32b0ebeb8f0ee63b9
dominic-source <chinonsodomnic@gmail.com>
2023-10-10 10:33:00 +0100

    linear search

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,15 +1,27 @@
 #include "search_algos.h"
 
+
+/**
+ * linear_search - find a number from a list of integers/numbers
+ *
+ * @array: this is the array of integers
+ * @size: the size of the array
+ * @value: the value in search of
+ * Return: 1 or -1
+ */
 int linear_search(int *array, size_t size, int value)
 {
-  size_t i = 0;
+	size_t i = 0;
 
-  for (i = 0; i < size; i++)
-    {
-    
+	if (array == NULL)
+		return (-1);
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array[%li] = [%i]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
 
+	}
 
-
-
-
+	return (-1);
 }
